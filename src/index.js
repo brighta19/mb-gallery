df2bb7994a7f5db0459c4d0601045d2cf151497e
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2022-07-21 17:26:44 -0400

    Router

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import { createRoot } from "react-dom/client";
 
 //Pages
 import Landing from "./views/Landing.jsx";
+import Upload from "./views/Upload.jsx";
 
 //Global Context
 import GlobalContext from "./GlobalContext.jsx";
+
+//Router
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const Main = () => {
   return (
     <>
       <GlobalContext>
-        <Landing />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/post" element={<Upload />} />
+          </Routes>
+        </BrowserRouter>
       </GlobalContext>
     </>
   );
---
+++ src/views/Upload.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState, useContext } from "react";
+
+//Styles
+import "../style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+
+const Upload = () => {
+  return (
+    <>
+      <h1>Hello World Upload</h1>
+    </>
+  );
+};
+
+export default Upload;
